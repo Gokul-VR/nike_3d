0f5809a38f0d4d4a0955bded95a4828f80ab7a60
Gokul VR <106941614+Gokul-VR@users.noreply.github.com>
2025-05-06 18:15:04 +0530

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import nike from '../public/nike-logo.png'
+
+export default function Sidebar() {
+  return (
+    <div className="flex justify-between mx-32 mt-7 items-center text-white">
+      <img src={nike} className="h-8" />
+      <div className="flex gap-5 items-center">
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="fill-white h-5 w-5"
+        >
+          <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+        </svg> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="fill-white h-5 w-5"
+        >
+          <path d="M21 4H3a1 1 0 0 0-1 1v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V5a1 1 0 0 0-1-1zm-9 9c-3.309 0-6-2.691-6-6h2c0 2.206 1.794 4 4 4s4-1.794 4-4h2c0 3.309-2.691 6-6 6z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="fill-white h-6 w-6"
+        >
+          <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+        </svg>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/AirMagPage.jsx
@@ -0,0 +1,113 @@
+import React, { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import AirMag from "../components/AirMag";
+import Loader from "../components/Loader";
+import Review from "../components/Review";
+
+export default function AirMagPage() {
+  return (
+    <div className="text-white">
+      <div className="mx-10 my-5  md:flex gap-8">
+        <div className="h-96 md:h-[600px] w-full md:w-[600px] relative ">
+          <p className="absolute text-9xl md:text-[200px]  font-outline-2 text-transparent whitespace-nowrap top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Airforce
+          </p>
+          <Canvas className="">
+            <OrbitControls
+              enableZoom={true}
+            // autoRotate={true}
+            // autoRotateSpeed={5.0}
+            />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[0, 15, 5]} intensity={2} />
+            <Suspense fallback={<Loader />}>
+              <AirMag />
+            </Suspense>
+          </Canvas>
+        </div>
+        <div className="  md:ml-24 mb-8 md:mt-24">
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-white"
+            >
+              <path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-white"
+            >
+              <path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-white"
+            >
+              <path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-white"
+            >
+              <path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-white"
+            >
+              <path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
+            </svg>
+          </div>
+          <div className=" uppercase">
+            <h4 className="mt-1 font-extralight">Limited Edition</h4>
+            <div className="flex justify-between items-center md:items-start md:flex-col ">
+              <h2 className="text-5xl md:text-8xl md:-mt-1 font-shoes ">
+                NIKE AIR FORCE 07
+              </h2>
+              <p className="text-3xl md:text-5xl -mt-2 md:-mt-4">$76,927</p>
+            </div>
+          </div>
+          <div className=" mt-5">
+            <p className="font-extralight">Size</p>
+            <div className="flex gap-3 mt-2">
+              <div className="border rounded-full w-fit hover:text-black hover:bg-white">
+                <p className="py-2 px-3 ">37</p>
+              </div>
+              <div className="border rounded-full w-fit hover:text-black hover:bg-white">
+                <p className="py-2 px-3 ">39</p>
+              </div>
+              <div className="border rounded-full w-fit hover:text-black hover:bg-white">
+                <p className="py-2 px-3 ">40</p>
+              </div>
+              <div className="border rounded-full w-fit hover:text-black hover:bg-white">
+                <p className="py-2 px-3 ">41</p>
+              </div>
+              <div className="border rounded-full w-fit hover:text-black hover:bg-white">
+                <p className="py-2 px-3 ">42</p>
+              </div>
+            </div>
+          </div>
+          <button className="bg-white text-black w-full py-3 mt-7 text-xl font-bold hover:bg-black hover:border hover:text-white rounded">
+            BUY NOW
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
