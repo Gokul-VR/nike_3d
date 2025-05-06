7ea6b0a6fd8a9870849ae555ccb69e174f90d65b
Gokul VR <106941614+Gokul-VR@users.noreply.github.com>
2025-05-06 18:53:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/drive': {
-        target: 'https://drive.google.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/drive/, '/uc'),
-      }
-    }
-  }
+  assetsInclude: ['**/*.glb']
 })
